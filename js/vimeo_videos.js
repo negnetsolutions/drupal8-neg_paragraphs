fc60eed6c4c6168e046f3d721af8e372c6fb57ac
Andrew Johnson <andy@negnetsolutions.com>
2019-04-17 20:01:28 -0500

    removed depends on jquery for videos

--- js/vimeo_videos.js
+++ js/vimeo_videos.js
@@ -1,45 +1,44 @@
-(function($) {
-  $(document).ready(function(){
-    $('.paragraph-video.vimeo').each(function(){
-      var iframe = $('iframe',this).get(0);
-      var player = new Vimeo.Player(iframe);
-      var poster = $('.poster', this);
-      var playbtn = $('.poster button', this);
+(function() {
 
-      var setPosterVisible = function(visible,showPoster){
-        if(visible == true){
-          poster.addClass('visible');
-        }
-        else {
-          poster.removeClass('visible');
-        }
+  var video_paragraphs = document.querySelectorAll(".paragraph-video.vimeo");
+  Array.prototype.forEach.call(video_paragraphs, function(el) {
+    var iframe = el.querySelector('iframe');
+    var player = new Vimeo.Player(iframe);
+    var poster = el.querySelector('.poster');
+    var playbtn = el.querySelector('.poster button');
 
-        if(showPoster == false){
-          poster.addClass('transparent');
-        }
-        else {
-          poster.removeClass('transparent');
-        }
+    var setPosterVisible = function(visible,showPoster){
+      if(visible == true){
+        poster.classList.add('visible');
+      }
+      else {
+        poster.classList.remove('visible');
       }
 
-      player.on('play', function(){
-        setPosterVisible(false);
-      });
+      if(showPoster == false){
+        poster.classList.add('transparent');
+      }
+      else {
+        poster.classList.remove('transparent');
+      }
+    }
 
-      player.on('pause', function(){
-        setPosterVisible(true,false);
-      });
-
-      player.on('ended', function(){
-        setPosterVisible(true,true);
-      });
-
-      poster.on('click',function(){
-        player.play();
-      });
-
-      setPosterVisible(true,true);
-
+    player.on('play', function () {
+      setPosterVisible(false);
     });
+
+    player.on('pause', function () {
+      setPosterVisible(true,false);
+    });
+
+    player.on('ended', function () {
+      setPosterVisible(true,true);
+    });
+
+    poster.addEventListener('click', function () {
+      player.play();
+    });
+
+    setPosterVisible(true,true);
   });
-})(jQuery);
+})();
