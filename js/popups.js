625098ac8adafbe1f886206b1abf1cd5a3cd2684
Andrew Johnson <andy@negnet.co>
2020-08-06 12:34:24 -0500

    Full support for youtu.be in lightbox.

--- js/popups.js
+++ js/popups.js
@@ -6,20 +6,27 @@
       removalDelay: 160,
       preloader: false,
       fixedContentPos: false,
-      patterns: {
-        youtube: {
-          index: "youtube.com/",
-          id(url) {
-            const m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
-            if (!m || !m[1]) return null;
-            return m[1];
+      iframe: {
+        patterns: {
+          youtue: {
+            index: "youtu.be/",
+            id: "/",
+            src: "//www.youtube.com/embed/%id%?autoplay=1"
           },
-          src: "//www.youtube.com/embed/%id%?autoplay=1"
-        },
-        vimeo: {
-          index: "vimeo.com/",
-          id: "/",
-          src: "//player.vimeo.com/video/%id%"
+          youtube: {
+            index: "youtube.com/",
+            id(url) {
+              const m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+              if (!m || !m[1]) return null;
+              return m[1];
+            },
+            src: "//www.youtube.com/embed/%id%?autoplay=1"
+          },
+          vimeo: {
+            index: "vimeo.com/",
+            id: "/",
+            src: "//player.vimeo.com/video/%id%"
+          }
         }
       }
     });
